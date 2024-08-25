e8fdba218d4226f99c099230164fdf6abd8a3ced
Josue-AV <jav0826@gmail.com>
2024-08-24 21:43:59 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+
+
 function encriptar(){
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
     let muñeco = document.getElementById("muñeco"); 
 
+
     let textoCifrado = texto
         .replace(/e/gi, "enter")
-        .replace(/i/gi, "ines")
+        .replace(/i/gi, "imes")
         .replace(/a/gi, "ai")
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat")
 
     if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
+
         tituloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent = "";
         muñeco.src = "img/encriptado.jpg";
@@ -23,6 +27,7 @@ function encriptar(){
         swal("Oops!","Debes ingresar un texto","warning");
 
         }
+
     }
     function desencriptar() {
         let texto = document.getElementById("texto").value;
@@ -32,7 +37,7 @@ function encriptar(){
 
         let textoCifrado = texto
             .replace(/enter/gi, "e")
-            .replace(/ines/gi, "i")
+            .replace(/imes/gi, "i")
             .replace(/ai/gi, "a")
             .replace(/ober/gi, "o")
             .replace(/ufat/gi, "u")
@@ -48,4 +53,9 @@ function encriptar(){
             parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
             swal("Oops!","Debes ingresar un texto","warning");
         }
- }
+
+       
+            
+        }
+    
+ 
